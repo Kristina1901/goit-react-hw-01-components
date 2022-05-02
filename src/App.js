63b33ed0b08636c07d5f28b0d4c263630c73ae2f
Kristina <ktuzova1901@gmail.com>
2022-05-02 19:39:35 +0300

    prettier

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import './App.css';
-import Profile from './components/Profile/Profile'
-import user from './Data/user.json'
-import data from './Data/data.json'
-import StatisticsList from './components/Statistic/StatisticsList'
-import friends from './Data/friends.json'
-import FriendList from './components/FriendList/FriendList'
-import transactions from './Data/transition.json'
-import TransactionHistory from './components/TransactionHistory/TransactionHistory'
+import Profile from './components/Profile/Profile';
+import user from './Data/user.json';
+import data from './Data/data.json';
+import StatisticsList from './components/Statistic/StatisticsList';
+import friends from './Data/friends.json';
+import FriendList from './components/FriendList/FriendList';
+import transactions from './Data/transition.json';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 function App() {
-  return <div>
-    <Profile
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
-    />
-    <StatisticsList title="Upload stats" stats={data} />
-    <FriendList friends={friends} />
-    <TransactionHistory items={transactions} />
-   
-  </div>
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <StatisticsList title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </div>
+  );
 }
 
 export default App;
